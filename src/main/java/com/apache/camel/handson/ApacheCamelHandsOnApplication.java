959521c72aaafd69b3ab751e6e54c967d4bad769
zeeroiq <shrikantvishwakarma@outlook.com>
2021-12-14 13:39:44 +0530

    project initialization files. close #1

---
+++ src/main/java/com/apache/camel/handson/ApacheCamelHandsOnApplication.java
@@ -0,0 +1,13 @@
+package com.apache.camel.handson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCamelHandsOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCamelHandsOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apache/camel/handson/ApacheCamelHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apache.camel.handson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheCamelHandsOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
